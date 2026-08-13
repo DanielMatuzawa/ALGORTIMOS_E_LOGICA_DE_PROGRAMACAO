db60943c74800a028eb083c61079fea6fabf09f9
Daniel Matuzawa <danielmatuzawa@gmail.com>
2026-08-13 16:18:27 -0300

    Add disciplines for semesters 4 to 8

--- media de notas.py
+++ media de notas.py
@@ -140,6 +140,192 @@ elif semestre == "3. semestre":
             disciplina = "Programação Front End"
         else: 
             print("Opção Inválida")
+            
+elif semestre == "4. semestre":
+
+    while True:
+        print("\n=== DISCIPLINAS DO QUARTO SEMESTRE ===")
+        print("1. Banco de Dados")
+        print("2. Engenharia de Requisitos")
+        print("3. Estudo Contemporâneo e Transversal: Indústria e Transformação Digital")
+        print("4. Estudo Contemporâneo e Transversal: Inovação e Pensamento Criativo")
+        print("5. Programação Orientada a Objetos")
+        print("6. Sistemas Operacionais")
+
+        disciplina = input("\nInsira sua disciplina: ")
+
+        if disciplina == "1":
+            disciplina = "Banco de Dados"
+            break
+        elif disciplina == "2":
+            disciplina = "Engenharia de Requisitos"
+            break
+        elif disciplina == "3":
+            disciplina = "Estudo Contemporâneo e Transversal: Indústria e Transformação Digital"
+            break
+        elif disciplina == "4":
+            disciplina = "Estudo Contemporâneo e Transversal: Inovação e Pensamento Criativo"
+            break
+        elif disciplina == "5":
+            disciplina = "Programação Orientada a Objetos"
+            break
+        elif disciplina == "6":
+            disciplina = "Sistemas Operacionais"
+            break
+        else:
+            print("Opção Inválida")
+
+
+elif semestre == "5. semestre":
+
+    while True:
+        print("\n=== DISCIPLINAS DO QUINTO SEMESTRE ===")
+        print("1. Estruturas, Pesquisa e Ordenação de Dados")
+        print("2. Estudo Contemporâneo e Transversal: Leitura de Imagens, Gráficos e Mapas")
+        print("3. Estudo Contemporâneo e Transversal: Propriedade Intelectual")
+        print("4. Interface Humano-Computador")
+        print("5. Libras")
+        print("6. Manutenção de Software")
+        print("7. Programação Avançada")
+
+        disciplina = input("\nInsira sua disciplina: ")
+
+        if disciplina == "1":
+            disciplina = "Estruturas, Pesquisa e Ordenação de Dados"
+            break
+        elif disciplina == "2":
+            disciplina = "Estudo Contemporâneo e Transversal: Leitura de Imagens, Gráficos e Mapas"
+            break
+        elif disciplina == "3":
+            disciplina = "Estudo Contemporâneo e Transversal: Propriedade Intelectual"
+            break
+        elif disciplina == "4":
+            disciplina = "Interface Humano-Computador"
+            break
+        elif disciplina == "5":
+            disciplina = "Libras"
+            break
+        elif disciplina == "6":
+            disciplina = "Manutenção de Software"
+            break
+        elif disciplina == "7":
+            disciplina = "Programação Avançada"
+            break
+        else:
+            print("Opção Inválida")
+
+
+elif semestre == "6. semestre":
+
+    while True:
+        print("\n=== DISCIPLINAS DO SEXTO SEMESTRE ===")
+        print("1. Banco de Dados NoSQL")
+        print("2. Estudo Contemporâneo e Transversal: Direitos Humanos, Cidadania e Inclusão")
+        print("3. Estudo Contemporâneo e Transversal: Relações Étnico-Raciais e Diferentes Culturas")
+        print("4. Paradigmas de Linguagens de Programação")
+        print("5. Processos de Software")
+        print("6. Projeto, Implementação e Teste de Software")
+
+        disciplina = input("\nInsira sua disciplina: ")
+
+        if disciplina == "1":
+            disciplina = "Banco de Dados NoSQL"
+            break
+        elif disciplina == "2":
+            disciplina = "Estudo Contemporâneo e Transversal: Direitos Humanos, Cidadania e Inclusão"
+            break
+        elif disciplina == "3":
+            disciplina = "Estudo Contemporâneo e Transversal: Relações Étnico-Raciais e Diferentes Culturas"
+            break
+        elif disciplina == "4":
+            disciplina = "Paradigmas de Linguagens de Programação"
+            break
+        elif disciplina == "5":
+            disciplina = "Processos de Software"
+            break
+        elif disciplina == "6":
+            disciplina = "Projeto, Implementação e Teste de Software"
+            break
+        else:
+            print("Opção Inválida")
+
+
+elif semestre == "7. semestre":
+
+    while True:
+        print("\n=== DISCIPLINAS DO SÉTIMO SEMESTRE ===")
+        print("1. Estudo Contemporâneo e Transversal: Protagonismo e Autonomia Intelectual Criativa")
+        print("2. Estudo Contemporâneo e Transversal: Relação de Consumo e Sustentabilidade")
+        print("3. Experiência Profissional: Desenvolvimento de Aplicações")
+        print("4. Inteligência Artificial")
+        print("5. Programação para Dispositivos Móveis")
+        print("6. Segurança e Auditoria de Sistemas")
+        print("7. Teoria da Computação")
+
+        disciplina = input("\nInsira sua disciplina: ")
+
+        if disciplina == "1":
+            disciplina = "Estudo Contemporâneo e Transversal: Protagonismo e Autonomia Intelectual Criativa"
+            break
+        elif disciplina == "2":
+            disciplina = "Estudo Contemporâneo e Transversal: Relação de Consumo e Sustentabilidade"
+            break
+        elif disciplina == "3":
+            disciplina = "Experiência Profissional: Desenvolvimento de Aplicações"
+            break
+        elif disciplina == "4":
+            disciplina = "Inteligência Artificial"
+            break
+        elif disciplina == "5":
+            disciplina = "Programação para Dispositivos Móveis"
+            break
+        elif disciplina == "6":
+            disciplina = "Segurança e Auditoria de Sistemas"
+            break
+        elif disciplina == "7":
+            disciplina = "Teoria da Computação"
+            break
+        else:
+            print("Opção Inválida")
+
+
+elif semestre == "8. semestre":
+
+    while True:
+        print("\n=== DISCIPLINAS DO OITAVO SEMESTRE ===")
+        print("1. Arquitetura de Software")
+        print("2. Estudo Contemporâneo e Transversal: Administração de Conflito")
+        print("3. Estudo Contemporâneo e Transversal: Inteligência Emocional")
+        print("4. Experiência Profissional: Fábrica de Software")
+        print("5. Gestão de Projetos Tecnológicos")
+        print("6. Qualidade de Software")
+        print("7. Tecnologias Emergentes em Engenharia de Software")
+
+        disciplina = input("\nInsira sua disciplina: ")
+
+        if disciplina == "1":
+            disciplina = "Arquitetura de Software"
+            break
+        elif disciplina == "2":
+            disciplina = "Estudo Contemporâneo e Transversal: Administração de Conflito"
+            break
+        elif disciplina == "3":
+            disciplina = "Estudo Contemporâneo e Transversal: Inteligência Emocional"
+            break
+        elif disciplina == "4":
+            disciplina = "Experiência Profissional: Fábrica de Software"
+            break
+        elif disciplina == "5":
+            disciplina = "Gestão de Projetos Tecnológicos"
+            break
+        elif disciplina == "6":
+            disciplina = "Qualidade de Software"
+            break
+        elif disciplina == "7":
+            disciplina = "Tecnologias Emergentes em Engenharia de Software"
+            break
+        else:
+            print("Opção Inválida")
 
 
 ###NOTAS### 
@@ -196,3 +382,6 @@ elif situação == "RECUPERAÇÃO!":
         print(f"\nInfelizmente {nome}, Terá que fazer a recuperação")
 else: 
         print(f"\n{nome}... voce está reprovado, tente novamente quando possível")
+
+
+
